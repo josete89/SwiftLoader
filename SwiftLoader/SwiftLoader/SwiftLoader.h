137baf275bc670e8262badd41c53d6465f5d109a
josete89 <josete_zgz_89@hotmail.com>
2016-04-02 12:09:01 +0200

    Carthage support

---
+++ SwiftLoader/SwiftLoader/SwiftLoader.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLoader.h
+//  SwiftLoader
+//
+//  Created by Jose Luis Alcala Ramos on 2/4/16.
+//  Copyright © 2016 com.kunst.SwiftLoader. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftLoader.
+FOUNDATION_EXPORT double SwiftLoaderVersionNumber;
+
+//! Project version string for SwiftLoader.
+FOUNDATION_EXPORT const unsigned char SwiftLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLoader/PublicHeader.h>
+
+
